a93ad5764d60ea02b5a26f6f7854841fe9d68975
Oleg <serv_all@mail.ru>
2022-03-14 23:07:34 +0300

    Введен класс Sheet с сохранением в БД Добавлено SheetDTO для формирования Sheet

---
+++ DBLibrary/Entityes/Sheet.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBLibrary
+{
+    public class Sheet
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Closed { get; set; }
+        public bool IsApproved { get; set; }
+        public int User { get; set; }
+        public int Employee { get; set; }
+        public decimal Amount { get; set; }
+
+        public Sheet()
+        {
+
+        }
+
+        private Sheet(int user, int employee, string description)
+        {
+            Created = DateTime.Now;
+            User = user;
+            Employee = employee;
+            Description = description;
+        }
+
+        public static Sheet Create(int userOwner, int employee, string description)
+        {
+            return new Sheet(userOwner, employee, description);
+        }
+
+        public void Approve(decimal amount)
+        {
+            Amount = amount;
+            Closed = DateTime.Now;
+            IsApproved = true;
+        }
+
+        public void Reopen()
+        {
+            Amount = 0;
+            Closed = null;
+            IsApproved = false;
+        }
+
+    }
+}
---
+++ DBLibrary/ISheetDBRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBLibrary
+{
+    public interface ISheetDBRepository
+    {
+        Task Create(SheetDTO sheetDTO);
+        Task<Sheet> Get(int id);
+        Task Approve(int id, decimal amount);
+        Task Reopen(int id);
+    }
+}
